c48028a48ab806adb540d8d978f3a0d19e140519
Eli Craig <elidcraig@gmail.com>
2022-06-06 13:57:45 -0700

    commit tree 74e6f28f29ec5d79a8bf91448bf89c4fa22f9e11

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker name='<Your Name>'
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator speaker_names
+  speaker_names.map { |name| badge_maker name}
+end  
+
+def assign_rooms speaker_names
+  assignments = []
+  speaker_names.each_with_index do |name, index|
+    assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end 
+  assignments
+end   
+
+def printer attendees
+  attendees.each { |name| puts badge_maker(name) }
+  assign_rooms(attendees).each {|message| puts message}
+end  
